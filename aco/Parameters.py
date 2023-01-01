4f02ec5f6ef766aa15582ea866367974286fbc26
KITA-DS12 <kita.ds12@gmail.com>
2023-01-01 14:25:38 +0900

    fix Parmeters.py

--- aco/Parameters.py
+++ aco/Parameters.py
@@ -1,3 +1,13 @@
 class Parameters:
     def __init__(self, num_ant, num_vertex, q, alpha, beta, rou,
-                 max_iteration, initial_vertex, min_tau, max_tau)
+                 max_iteration, initial_vertex, min_tau, max_tau):
+        self.num_ant = num_ant
+        self.num_vertex = num_vertex
+        self.q = q
+        self.alpha = alpha
+        self.beta = beta
+        self.rou = rou
+        self.max_iteration = max_iteration
+        self.initial_vartex = initial_vertex
+        self.min_tau = min_tau
+        self.max_tau = max_tau
